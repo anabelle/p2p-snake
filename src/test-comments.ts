d4331bea743e0b46d8745a7b5ddefc886d78b38d
Anabelle Handdoek <git@huellaspyp.com>
2025-04-12 13:42:46 -0500

    chore: Remove test file and finalize strip-comments integration

--- src/test-comments.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-
-export function testFunction() {
-  return 'Test function';
-}
-
-// eslint-disable-next-line no-console
-console.log('This line has a comment that should be preserved');
